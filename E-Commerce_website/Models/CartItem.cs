eeed8c6359e0d9538524f6027b2d4ac1a0ea650f
Zachary <ZackCipolletta@gmail.com>
2023-04-03 14:01:26 -0700

    add models for ShoppingCart and CartItems to hold and track items added to cart.

---
+++ E-Commerce_website/Models/CartItem.cs
@@ -0,0 +1,15 @@
+
+
+
+namespace CsharpTeamWeek.Models
+{
+  public class CartItem
+  {
+    public int CartItemId { get; set; }
+    public int ShoppingCartId { get; set; }
+    public string Name { get; set; }
+    public string Price { get; set; }
+    public string ImageLink { get; set; }
+    
+  }
+}
---
+++ E-Commerce_website/Models/ShoppingCart.cs
@@ -0,0 +1,9 @@
+
+namespace CsharpTeamWeek.Models;
+
+public class ShoppingCart
+{
+    public int ShoppingCartId { get; set; }
+
+    public virtual List<CartItem> CartItems { get; set; }
+}
